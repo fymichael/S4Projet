371f409f12e04b52d6fb2607fa3ae4d29fa17899
fymichael <andrianiainafymichael@gmail.com>
2023-03-28 11:52:58 +0300

    A

--- build.bat
+++ build.bat
@@ -2,4 +2,3 @@ jar -cf fw.jar -C .\Framework\build\web\WEB-INF\classes\ etu1998
 jar -cvf test_fw.war -C .\Test-Framework\build\web\ WEB-INF\
 copy test_fw.war C:\MyWebServer\apache-tomcat-10.0.20\webapps\
 xcopy fw.jar C:\Users\P15B-79-FY\Documents\S4Projet\Test-Framework\build\web\WEB-INF\lib\
-xcopy C:\Users\P15B-79-FY\Documents\S4Projet\Test-Framework\build\web\WEB-INF\ C:\MyWebServer\apache-tomcat-10.0.20\webapps\Framework\WEB-INF\ /E
